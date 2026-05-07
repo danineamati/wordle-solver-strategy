680250114bb9e250c441557bdd97924c7ecb3e9f
danineamati <danineamati@gmail.com>
2026-05-06 20:23:52 -0600

    Update repo structure for pyproject.toml

--- demos/demo_solver.py
+++ demos/demo_solver.py
@@ -1,8 +1,15 @@
+import pathlib
+
 from wordle import MaxInfoSolver as Solver
 
-with open("../words_answers.txt", "r") as answers_file:
+CURR_FILE_DIR = pathlib.Path(__file__).parent
+ROOT_DIR = CURR_FILE_DIR.parent
+WORDS_ANSWERS_PATH = ROOT_DIR / "words_answers.txt"
+WORDS_GUESSES_PATH = ROOT_DIR / "words_guesses.txt"
+
+with open(WORDS_ANSWERS_PATH, "r") as answers_file:
     answers = answers_file.read().splitlines()
-with open("../words_guesses.txt", "r") as guesses_file:
+with open(WORDS_GUESSES_PATH, "r") as guesses_file:
     guesses = guesses_file.read().splitlines()
 
 solver = Solver(answers, guesses, mode="standard")
